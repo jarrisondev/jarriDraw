e4d2624764769fb1c3daa75d8b729c09b0f738ce
Jarrison Cano <jarrison.cano@hotmail.com>
2024-04-13 15:20:58 -0500

    feat: add link to the logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,14 @@
 import { Sun } from "lucide-react"
 import Image from "next/image"
 import { Button } from "./ui/button"
+import Link from "next/link"
 
 export default function Header() {
 	return (
 		<nav className="flex justify-between items-center">
-			<Image src="/logo.svg" alt="Vercel Logo" width={131} height={64} priority />
+			<Link href="/">
+				<Image src="/logo.svg" alt="Vercel Logo" width={131} height={64} priority />
+			</Link>
 			<div className="flex items-center gap-5">
 				<Button variant="ghost" size="icon">
 					<Sun />
