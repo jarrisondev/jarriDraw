3fbd886af60914201dc2c1acc768de6cd751a40c
Jarrison Cano <jarrison.cano@hotmail.com>
2024-04-13 15:16:45 -0500

    feat: add header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,20 @@
+import { Sun } from "lucide-react"
 import Image from "next/image"
+import { Button } from "./ui/button"
 
 export default function Header() {
 	return (
-		<nav>
-			<Image src="/logo.svg" alt="Vercel Logo" width={121} height={54} priority />
-			JarriDraw
-			<p>Hello</p>
+		<nav className="flex justify-between items-center">
+			<Image src="/logo.svg" alt="Vercel Logo" width={131} height={64} priority />
+			<div className="flex items-center gap-5">
+				<Button variant="ghost" size="icon">
+					<Sun />
+				</Button>
+				<Button className="flex gap-3">
+					Log in
+					<Image src="/icons/github.svg" alt="Vercel Logo" width={22} height={22} priority />
+				</Button>
+			</div>
 		</nav>
 	)
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "./footer"
 import Header from "./header"
 
 export default function Layout({
@@ -6,9 +7,10 @@ export default function Layout({
 	children: React.ReactNode
 }>) {
 	return (
-		<main className="border border-red-50 max-w-7xl mx-auto flex flex-col min-h-screen">
+		<main className="border border-red-50 max-w-7xl mx-auto flex flex-col justify-between min-h-screen p-5">
 			<Header />
 			{children}
+			<Footer />
 		</main>
 	)
 }
