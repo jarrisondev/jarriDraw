7517951a286f8c6f9741fb938abf89eefd16dd2c
Jarrison Cano <jarrison.cano@hotmail.com>
2024-04-13 16:38:48 -0500

    feat: add hero

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,18 @@
+import { cn } from "@/lib/utils"
 import Footer from "./footer"
 import Header from "./header"
 
 export default function Layout({
 	children,
+	className,
 }: Readonly<{
 	children: React.ReactNode
+	className?: string
 }>) {
 	return (
-		<main className="border border-red-50 max-w-7xl mx-auto flex flex-col justify-between min-h-screen p-5">
+		<main className="max-w-7xl mx-auto flex flex-col justify-between min-h-screen p-5">
 			<Header />
-			<section className="flex-1">{children}</section>
+			<section className={cn("flex-1", className)}>{children}</section>
 			<Footer />
 		</main>
 	)
