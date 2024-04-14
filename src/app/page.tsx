faf861811551d793e4cf8a85801312f05d726ec0
Jarrison Cano <jarrison.cano@hotmail.com>
2024-04-14 03:02:41 -0500

    feat: add information section to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import HomeCard from "@/components/Home/homeCard"
 import { Icons } from "@/components/icons"
 import Layout from "@/components/Layout/layout"
 import { Button } from "@/components/ui/button"
@@ -5,26 +6,41 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
 	return (
 		<Layout>
-			<div className="m-32 text-center flex flex-col items-center gap-3">
-				<h1 className="font-extrabold text-7xl">
-					Next-Level <br />{" "}
-					<span className="bg-gradient-to-r from-[#FF0000] from-15% to-[#FF8A00] text-transparent bg-clip-text">
-						Excalidraw
-					</span>{" "}
-					experience
-				</h1>
-				<p className="text-cGray text-xl font-extralight w-4/5">
-					Unlock your creativity with our advanced Excalidraw tool. Save, manage, and share multiple
-					boards effortlessly.
-				</p>
-				<Button
-					size="lg"
-					variant="outline"
-					className="mt-7 flex gap-3 text-base dark:bg-[#222] dark:hover:bg-[#1a1a1a] border-[#4E4E4E]"
-				>
-					<Icons.gitHub className="h-5 w-5" />
-					Continue with GitHub
-				</Button>
+			<div className="my-36">
+				<div className="mb-44 text-center flex flex-col items-center gap-3">
+					<h1 className="font-extrabold text-7xl">
+						Next-Level <br />{" "}
+						<span className="bg-gradient-to-r from-[#FF0000] from-15% to-[#FF8A00] text-transparent bg-clip-text">
+							Excalidraw
+						</span>{" "}
+						experience
+					</h1>
+					<p className="text-cGray text-xl font-extralight w-4/5">
+						Unlock your creativity with our advanced Excalidraw tool. Save, manage, and share
+						multiple boards effortlessly.
+					</p>
+					<Button
+						size="lg"
+						variant="outline"
+						className="mt-7 flex gap-3 text-base dark:bg-[#222] dark:hover:bg-[#1a1a1a] border-[#4E4E4E]"
+					>
+						<Icons.gitHub className="h-5 w-5" />
+						Continue with GitHub
+					</Button>
+				</div>
+				<section className="flex flex-col gap-44 mt-52">
+					<HomeCard
+						title="Limitless Drawing"
+						description="Unlock limitless creativity with our powerful Excalidraw editor seamlessly integrated into your workflow. Create dynamic diagrams, sketches, and visual notes with ease."
+						imageUrl="/images/home-1.jpg"
+					/>
+					<HomeCard
+						title="Your Projects, One Click Away"
+						description="View and manage all your boards and projects in a clear, structured manner. Stay on top of your work and enhance your productivity."
+						imageUrl="/images/home-2.jpg"
+						reverse
+					/>
+				</section>
 			</div>
 		</Layout>
 	)
---
+++ src/components/Home/homeCard.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+
+interface Props {
+	title: string
+	description: string
+	imageUrl: string
+	reverse?: boolean
+	className?: string
+}
+
+export default function HomeCard({ title, description, imageUrl, reverse, className }: Props) {
+	return (
+		<div
+			className={cn("flex justify-between items-center gap-16", className, {
+				"flex-row-reverse": reverse,
+			})}
+		>
+			<Image src={imageUrl} alt="Image of preview" width={550} height={500} />
+			<div className="flex flex-col gap-14">
+				<h3 className="text-5xl font-bold">{title}</h3>
+				<p className="text-lg font-light text-cGray leading-9">{description}</p>
+			</div>
+		</div>
+	)
+}
