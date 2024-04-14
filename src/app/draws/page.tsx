4ab03db011a645fab5c7c495373c3c6c72a32591
Jarrison Cano <jarrison.cano@hotmail.com>
2024-04-14 13:11:15 -0500

    feat: add draws page

---
+++ src/app/draws/page.tsx
@@ -0,0 +1,14 @@
+import HomeCard from "@/components/Home/homeCard"
+import { Icons } from "@/components/icons"
+import Layout from "@/components/Layout/layout"
+import { Button } from "@/components/ui/button"
+
+export default function Draws() {
+	return (
+		<Layout>
+			<div>
+				<h1>Hello world</h1>
+			</div>
+		</Layout>
+	)
+}
