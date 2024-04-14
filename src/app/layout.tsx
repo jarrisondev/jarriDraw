5fb3a0cdf231bb90e93ad3041650db43eb0e9cc9
Jarrison Cano <jarrison.cano@hotmail.com>
2024-04-14 14:17:17 -0500

    add Avatar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { ReactQueryClientProvider } from "@/components/reactQueryClientProvider"
 
