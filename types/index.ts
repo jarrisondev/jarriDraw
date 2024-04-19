c87d426e706fad8a5eb24781e56810b6b9587789
Jarrison Cano <jarrison.cano@hotmail.com>
2024-04-19 00:32:27 -0500

    Add types

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,8 @@
 import { Database } from "./supabase"
 
 export type User = Database["public"]["Tables"]["profiles"]["Row"]
+
+export type Board = Database["public"]["Tables"]["board"]["Row"]
+export type InsertBoard = Database["public"]["Tables"]["board"]["Insert"]
+
+export type BoardUser = Database["public"]["Tables"]["board_user"]["Row"]
